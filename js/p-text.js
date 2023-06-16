1e7b55afc6769b68fe11251934ecf47188518183
shangchu0412 <shangchu0412@gmail.com>
2023-06-16 17:13:55 +0800

    測試版-模擬考試

--- js/p-text.js
+++ js/p-text.js
@@ -1,12 +1,13 @@
-$(document).ready(function() {
+$(document).ready(function () {
   var topButton = $('.top');
   var navLinks = $('.header .nav ul li a');
+  let headerTop = $('.header h1');
 
   // 初始时隐藏按钮
   topButton.hide();
 
   // 监听页面滚动事件
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     // 获取滚动距离
     var scrollPos = $(window).scrollTop();
 
@@ -18,7 +19,7 @@ $(document).ready(function() {
     }
 
     // 遍历每个导航链接
-    navLinks.each(function() {
+    navLinks.each(function () {
       var target = $(this).attr('href');
       var targetElement = $(target);
 
@@ -27,7 +28,7 @@ $(document).ready(function() {
       var targetHeight = targetElement.outerHeight();
 
       // 判断滚动位置是否在目标区块内
-      if (scrollPos >= targetPosition - 200 && scrollPos < targetPosition + targetHeight -200) {
+      if (scrollPos >= targetPosition - 200 && scrollPos < targetPosition + targetHeight - 200) {
         // 添加选中样式
         $(this).addClass('active');
       } else {
@@ -38,7 +39,7 @@ $(document).ready(function() {
   });
 
   // 监听导航链接的点击事件
-  navLinks.click(function(event) {
+  navLinks.click(function (event) {
     event.preventDefault();
     var targetId = $(this).attr('href');
     var targetElement = $(targetId);
@@ -46,7 +47,7 @@ $(document).ready(function() {
 
     // 平滑滚动到目标位置
     $('html, body').animate({
-      scrollTop: targetOffset -150
+      scrollTop: targetOffset - 150
     }, 'slow');
 
     // 添加选中样式
@@ -55,11 +56,19 @@ $(document).ready(function() {
   });
 
   // 点击按钮时滚动到顶部
-  topButton.click(function() {
+  topButton.click(function () {
     $('html, body').animate({
       scrollTop: 0
     }, 'slow');
   });
+
+  // 点击按钮时滚动到顶部
+  headerTop.click(function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 'slow');
+  });
+
 });
 
 
@@ -67,13 +76,13 @@ $(document).ready(function() {
 
 
 // 抓取header位置讓wrap區塊緊貼其後
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
   var headerHeight = document.querySelector('.header').offsetHeight;
   var wrapElement = document.querySelector('.wrap');
   wrapElement.style.marginTop = headerHeight + 'px';
 });
 
-window.addEventListener('resize', function() {
+window.addEventListener('resize', function () {
   var headerHeight = document.querySelector('.header').offsetHeight;
   var wrapElement = document.querySelector('.wrap');
   wrapElement.style.marginTop = headerHeight + 'px';
@@ -82,7 +91,7 @@ window.addEventListener('resize', function() {
 
 
 // 視窗滾動時，header內字體縮放達成header區塊縮小
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   var header = document.querySelector('.header');
   var headerTitle = document.querySelector('.header h1');
   var navLinks = document.querySelectorAll('.header .nav ul li a');
@@ -96,39 +105,87 @@ window.addEventListener('scroll', function() {
     headerTitle.style.fontSize = '24px';
 
     // 遍历所有匹配的链接元素并应用样式
-    navLinks.forEach(function(link) {
+    navLinks.forEach(function (link) {
       link.style.fontSize = '20px';
     });
   } else {
     header.style.height = 'auto';
-    headerTitle.style.fontSize = '48px';
+    headerTitle.style.fontSize = '40px';
 
     // 遍历所有匹配的链接元素并应用样式
-    navLinks.forEach(function(link) {
+    navLinks.forEach(function (link) {
       link.style.fontSize = '32px';
     });
   }
 });
 
+// 获取 repeat 元素
+var repeatElement = document.getElementById('repeat');
 
-// nav選單點擊後平滑滾動到區塊位置
-window.addEventListener('DOMContentLoaded', function() {
-  var navLinks = document.querySelectorAll('.header .nav ul li a');
+// 获取所有的 audio 元素
+var audioElements = document.querySelectorAll('.pronounce audio');
 
-  // 监听导航链接的点击事件
-  for (var i = 0; i < navLinks.length; i++) {
-    navLinks[i].addEventListener('click', function(event) {
-      event.preventDefault();
-      var targetId = this.getAttribute('href');
-      var targetElement = document.querySelector(targetId);
-      var headerHeight = document.querySelector('.header').offsetHeight;
-      var targetOffset = targetElement.offsetTop - headerHeight;
+// 存储音频文件的数组
+var audioFiles = [];
 
-      // 平滑滚动到目标位置
-      window.scrollTo({
-        top: targetOffset,
-        behavior: 'smooth'
-      });
-    });
+// 将音频文件添加到数组中
+audioElements.forEach(function(audioElement) {
+  var audioSrc = audioElement.querySelector('source[type="audio/mpeg"]').src;
+  audioFiles.push(audioSrc);
+});
+
+// 随机排序音频文件数组
+audioFiles.sort(function() {
+  return 0.5 - Math.random();
+});
+
+// 当前播放的音频索引
+var currentAudioIndex = 0;
+
+// 记录是否处于暂停状态
+var isPaused = false;
+
+// 当前播放的音频元素
+var currentAudioElement;
+
+// 播放音频
+function playAudio() {
+  currentAudioElement = new Audio(audioFiles[currentAudioIndex]);
+
+  currentAudioElement.addEventListener('ended', function() {
+    // 当前音频播放完毕时，等待5秒后播放下一个音频
+    setTimeout(function() {
+      if (!isPaused) {
+        currentAudioIndex = (currentAudioIndex + 1) % audioFiles.length;
+        playAudio();
+      }
+    }, 5000);
+  });
+
+  currentAudioElement.play();
+}
+
+// 暂停音频
+function pauseAudio() {
+  if (currentAudioElement) {
+    currentAudioElement.pause();
+  }
+}
+
+// 为 repeat 元素添加点击事件监听器
+repeatElement.addEventListener('click', function() {
+  if (!isPaused) {
+    // 当前处于播放状态，点击按钮暂停播放
+    pauseAudio();
+    isPaused = true;
+    repeatElement.innerHTML = '<i class="material-symbols-outlined">repeat</i>';
+  } else {
+    // 当前处于暂停状态，点击按钮继续播放
+    isPaused = false;
+    playAudio();
+    repeatElement.innerHTML = '<i class="material-symbols-outlined">stop</i>';
   }
 });
+
+// 开始播放音频
+playAudio();
